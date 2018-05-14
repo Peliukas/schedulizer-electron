c9fdab985e6aa52eda211fd6b02eb799b39cb8e3
Peliukas <pavel.kocia@gmail.com>
2018-05-15 00:01:49 +0200

    Attempted to implement application updater;

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const {app, BrowserWindow} = require('electron');
+const updater = require('electron-simple-updater');
+updater.init({
+    checkUpdateOnStart: true,
+    autoDownload: false
+});
 let win;
 
 function createWindow() {
@@ -48,50 +53,3 @@ app.on('activate', function () {
         createWindow()
     }
 })
-
-//
-// const isDev = require('electron-is-dev');
-//
-// const {app, BrowserWindow, ipcMain} = require('electron');
-// const {autoUpdater} = require("electron-updater");
-// const server = 'https://github.com/peliukas/schedulizer-electron'
-// // const feed = `${server}/update/${process.platform}/${app.getVersion()}`
-// // autoUpdater.setFeedURL(server + '/' +process.platform + '/' + app.getVersion())
-// setInterval(() => {
-//     autoUpdater.checkForUpdates()
-// }, 60000)
-// let win; // this will store the window object
-//
-// // creates the default window
-// function createDefaultWindow() {
-//     win = new BrowserWindow({width: 900, height: 680});
-//     win.loadURL(`file://${__dirname}/dist/index.html`);
-//     win.on('closed', () => app.quit());
-//     return win;
-// }
-//
-// // when the app is loaded create a BrowserWindow and check for updates
-// app.on('ready', function () {
-//     createDefaultWindow()
-// });
-//
-// if (isDev) {
-//     autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-//         const dialogOpts = {
-//             type: 'info',
-//             buttons: ['Restart', 'Later'],
-//             title: 'Application Update',
-//             message: process.platform === 'win32' ? releaseNotes : releaseName,
-//             detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-//         }
-//
-//         dialog.showMessageBox(dialogOpts, (response) => {
-//             if (response === 0) autoUpdater.quitAndInstall()
-//         })
-//     })
-//
-//     autoUpdater.on('error', message => {
-//         console.error('There was a problem updating the application')
-//         console.error(message)
-//     })
-// }
